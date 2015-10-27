a5d55910b017dad67cc487d6782b3e4203515773
Alexander Laing <lainga@uvic.ca>
2015-10-26 17:28:04 -0700

    JS update: added JS date figures and yellow figure highlighting.

---
+++ src/gitpage.js
@@ -0,0 +1,6 @@
+var t=864e5;
+var n=(new Date).getTime();
+var g=Date.UTC(2015,9,3);
+var s=Date.UTC(2016,0,4);
+document.getElementById("gitage").innerHTML=String(Math.floor((n-g)/t))+" days";
+document.getElementById("ctdown").innerHTML=String(Math.floor((s-n)/t))+" days";
